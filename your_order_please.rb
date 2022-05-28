c0f3db742187db8b0b96018ade8242c62f85aa44
Will Schroeder <willjrschroeder@gmail.com>
2022-05-28 15:39:40 +0200

    finished practice problem

--- your_order_please.rb
+++ your_order_please.rb
@@ -19,7 +19,7 @@ Output string
 An array of the input string split on whitespace
 Array (or hash?) to store the intermediate of each word, its position in the original string, and its sorted position.
 
-I can likely use the map function to solve this
+I can likely use the map function to solve this # sike
 
 
 Algorithm:
@@ -34,11 +34,34 @@ I need to find the number within each word string and place it into a hash, with
 I need to map the hash containing the sorted position numbers and the word strings onto an array of the words in order.
 	Mapping the hash: 
 		Call sort_by on the hash and sort it by value (number)
-		Will return an array of the words in order
+		Will return an array of arrays, in order. First value of each array pair is the word, second is the value
 		
-I then need to combine this array into a string separated by whitespace.
+I then need to isloate the words and combine them into a string separated by whitespace.
+	Iterate through the outer array
+		Push the first element in each pair onto a new array
 
 I can return this string as the solution
 
-
 '''
+
+def string_sorter(unsorted_string)
+	unsorted_words = unsorted_string.split
+	unsorted_words_and_position = {}
+
+	unsorted_words.each do |word|
+		word.chars.each do |char|
+			if %w(1 2 3 4 5 6 7 8 9).include?(char)
+				unsorted_words_and_position[word] = char.to_i
+			end
+		end
+	end
+
+	sorted_words_and_position = unsorted_words_and_position.sort_by{|key, value| value}
+	sorted_words = []
+	sorted_words_and_position.each{|arr| sorted_words << arr[0]}
+	sorted_words.join(" ")
+end
+
+p string_sorter("4of Fo1r pe6ople g3ood th5e the2")
+p string_sorter("is2 Thi1s T4est 3a")
+p string_sorter("")
